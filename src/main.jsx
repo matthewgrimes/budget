e3b2525119218da0c810f6d9516955bf3ef6dcd3
Matt Grimes <8381534+matthewgrimes@users.noreply.github.com>
2021-09-27 03:17:25 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import {BudgetDataBase} from './App'
+ReactDOM.render(
+  <React.StrictMode>
+    <BudgetDataBase />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
