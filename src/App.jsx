669968c3790316bfa3fcd7fddcd700061765e52c
Matt Grimes <8381534+matthewgrimes@users.noreply.github.com>
2021-10-03 20:04:22 +0000

    can change categories

--- src/App.jsx
+++ src/App.jsx
@@ -75,16 +75,46 @@ export class App extends React.Component {
     let register = this.state.register;
     let budget = this.state.budget;
     if (field == 'Category'| field == 'Date'| field == 'Inflow'| field == 'Outflow') {
+      let master_category = register[id]['Category'].split(':')[0];
+      let sub_category = register[id]['Category'].split(':')[1];
+      let date = register[id]['Date'].split(' ')[0]+' '+register[id]['Date'].split(' ')[2];
+      budget[master_category][sub_category][date] = budget[master_category][sub_category][date] ? budget[master_category][sub_category][date] : {'Budgeted':Decimal(0),'Outflows':Decimal(0),'Balance':Decimal(0)};
       if (field == 'Outflow') {
-        let master_category = register[id]['Category'].split(':')[0];
-        let sub_category = register[id]['Category'].split(':')[1];
-        let date = register[id]['Date'].split(' ')[0]+' '+register[id]['Date'].split(' ')[2];
-        let old_value = new Decimal(register[id]['Outflow']);
-        budget[master_category][sub_category][date] = budget[master_category][sub_category][date] ? budget[master_category][sub_category][date] : {'Budgeted':Decimal(0),'Outflows':Decimal(0),'Balance':Decimal(0)};
+
+        let old_value = new Decimal(register[id]['Outflow'] ? register[id]['Outflow'] : 0);
+
         budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].minus(old_value);
         budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].plus(Decimal(value));
         budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].plus(old_value);
         budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].minus(Decimal(value));
+      }
+      else if (field == 'Inflow') {
+        let old_value = new Decimal(register[id]['Inflow'] ? register[id]['Inflow'] : 0);
+        console.log(budget[master_category][sub_category][date]);
+        console.log(budget[master_category][sub_category][date]['Outflows']);
+        console.log(old_value);
+
+        budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].plus(old_value);
+        budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].minus(Decimal(value));
+        budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].minus(old_value);
+        budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].plus(Decimal(value));
+      }
+      else if (field == 'Category') {
+        let new_master_category = value.split(':')[0];
+        let new_sub_category = value.split(':')[1];
+        let inflows = new Decimal(register[id]['Inflow'] ? register[id]['Inflow'] : 0);
+        let outflows = new Decimal(register[id]['Outflow'] ? register[id]['Outflow'] : 0);
+        budget[new_master_category][new_sub_category][date] = budget[new_master_category][new_sub_category][date] ? budget[new_master_category][new_sub_category][date] : {'Budgeted':Decimal(0),'Outflows':Decimal(0),'Balance':Decimal(0)};
+
+        budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].minus(outflows);
+        budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].plus(outflows);
+        budget[master_category][sub_category][date]['Outflows']=budget[master_category][sub_category][date]['Outflows'].plus(inflows);
+        budget[master_category][sub_category][date]['Balance']=budget[master_category][sub_category][date]['Balance'].minus(inflows);
+
+        budget[new_master_category][new_sub_category][date]['Outflows']=budget[new_master_category][new_sub_category][date]['Outflows'].plus(outflows);
+        budget[new_master_category][new_sub_category][date]['Balance']=budget[new_master_category][new_sub_category][date]['Balance'].minus(outflows);
+        budget[new_master_category][new_sub_category][date]['Outflows']=budget[new_master_category][new_sub_category][date]['Outflows'].minus(inflows);
+        budget[new_master_category][new_sub_category][date]['Balance']=budget[new_master_category][new_sub_category][date]['Balance'].plus(inflows);
       }
     }
     register[id][field]=value;
